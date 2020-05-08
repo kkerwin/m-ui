f7fc83153d16a96a2d72a7e37679d7c0a82055b6
kkerwin <kkerwin@yeah.net>
2020-05-08 09:24:25 +0800

    git desktop

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     return (
 		<div className="App">
 			<h1>mui</h1>
-			<Icon />
+			<Icon type='heart-fill' style={{ height:'2em', width:"2em", fill:"blue" }} />
 		</div>
     )
 }
--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import { ReactComponent as Right } from  '../../assets/svg/right.svg'
+import { ReactComponent as Left } from  '../../assets/svg/left.svg'
+import { ReactComponent as Up } from  '../../assets/svg/up.svg'
+import { ReactComponent as Down } from  '../../assets/svg/down.svg'
+import { ReactComponent as Close } from  '../../assets/svg/close.svg'
+import { ReactComponent as Check } from  '../../assets/svg/check.svg'
+
 import  './icon.less'
 
-const Icon = () => {
-    return <div className="icon">
-人一样
-    </div>
+
+interface IconProps {
+    type : string 
+    prefix ?: string 
+    className ?: string
+    style ?: React.CSSProperties
+}
+
+
+const defaultIconTypes = [ <Right key='right'/>, <Left key='left'/>, <Up key="up"/>, <Down key="down"/>, <Close key="close"/>, <Check key="check"/> ]
+
+const Icon:React.FC<IconProps> = ({ type, prefix, style }) => {
+    let isDefault = defaultIconTypes.find(f=>f.key === type)      
+    if(isDefault){
+        isDefault = React.cloneElement(isDefault as React.FunctionComponentElement<any> ,{ style })    
+    }
+    return (<i className="icon" style={{ color:'red' }}>         
+        { isDefault ? isDefault : <svg style={style}><use xlinkHref={`#${prefix}-${type}`}></use></svg> }
+    </i>)
+}
+
+Icon.defaultProps = {
+    
 }
 
 export default Icon
